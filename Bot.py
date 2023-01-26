389eeedc9d45d085fe860c2c215a81ea66c67ed8
Ysnsnss <123631904+Ysnsnss@users.noreply.github.com>
2023-01-26 13:06:44 +0300

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -1,10 +1,9 @@
 import telebot
 bot = telebot.TeleBot('5298905240:AAHkIzwAQb7X1wpZJD2v9neG8OxVFHzf64Y');
-# Функция, обрабатывающая команду /start
+#/start
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
     bot.send_message(m.chat.id, 'Привет, напиши что-нибудь')
-# Получение сообщений от юзера
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     bot.send_message(message.chat.id, ' ' + message.text)
